be6675128c644d9c4f77bbed8ad2ef583ebea083
Sistema Print <sistemaprintsp@gmail.com>
2026-06-13 23:49:42 -0300

    Corrige icones do PWA sem acento

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 ﻿// Service Worker - Controle de Patio Print
 // Desenvolvido por Ramalho Sistemas e Software
 
-const CACHE_NAME = 'patio-print-v6.3-app-icon-cache';
+const CACHE_NAME = 'patio-print-v6.4-pwa-icons-cache';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
@@ -10,7 +10,8 @@ const STATIC_ASSETS = [
   '/css/seminovos.css',
   '/js/seminovos.js',
   '/images/logo-print.png',
-  '/images/%C3%8Dcone.png',
+  '/images/icon-192.png',
+  '/images/icon-512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css',
   'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js',
